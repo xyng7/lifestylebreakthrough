68d78adb555f135272013d0edbddafe22591e7cd
xinyi <xyng7@student.monash.edu>
2012-11-30 16:15:57 +1100

    bake newsletters

---
+++ app/Test/Fixture/NewsletterFixture.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * NewsletterFixture
+ *
+ */
+class NewsletterFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'key' => 'primary'),
+		'title' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 50, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'content' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 100, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'user_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'index'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'user_id' => array('column' => 'user_id', 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'title' => 'Lorem ipsum dolor sit amet',
+			'content' => 'Lorem ipsum dolor sit amet',
+			'created' => '2012-11-30 05:51:13',
+			'user_id' => 1
+		),
+	);
+
+}
